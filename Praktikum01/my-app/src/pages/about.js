4c699dc024b56d4bca675a723fd3170d86fbd03f
Candra Ahmad Dani <candraahmaddani04@gmail.com>
2026-02-15 21:39:14 +0700

    P01 Tugas Praktikum

---
+++ Praktikum01/my-app/src/pages/about.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>Nama: Candra Ahmad Dani</p>
+      <p>NIM: 2341720187</p>
+      <p>Prodi: D4 Teknik Informatika</p>
+      <a href="/">Kembali</a>
+    </div>
+  )
+}
