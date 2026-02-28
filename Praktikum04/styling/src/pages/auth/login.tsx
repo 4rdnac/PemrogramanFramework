89f545b56cd365e99113b5104521f6e9a69f99a6
Candra Ahmad Dani <candraahmaddani04@gmail.com>
2026-02-28 23:12:14 +0700

    P04 Refactoring Struktur Project (Best Practice)

--- Praktikum04/styling/src/pages/auth/login.tsx
+++ Praktikum04/styling/src/pages/auth/login.tsx
@@ -1,24 +1,9 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import style from "./login.module.css";
-
-const HalamanLogin = () => {
-  const { push } = useRouter();
-
-  const handlerLogin = () => {
-    // Simulasi login berhasil
-    localStorage.setItem("isLogin", "true");
-
-    push("/");
-  };
-
+import TampilanLogin from "../views/auth/login/";
+const halamanLogin = () => {
   return (
-    <div className={style.login}>
-      <h1>Halaman Login</h1>
-      <button onClick={handlerLogin}>Login</button> <br />
-      <Link href="/auth/register">Ke Halaman Register</Link>
-    </div>
-  );
+    <>
+    <TampilanLogin />
+    </>);
 };
 
-export default HalamanLogin;
+export default halamanLogin;
