f91973fdfe12faddf579bfd0f5cc92b0319aadb7
Candra Ahmad Dani <candraahmaddani04@gmail.com>
2026-03-09 00:36:51 +0700

    P08 Implementasi getServerSideProps pada server.tsx

--- Laporan Praktikum/Praktikum08.md
+++ Laporan Praktikum/Praktikum08.md
@@ -13,3 +13,13 @@
 3. Jalankan browser : http://localhost:3000/produk/server
 
    ![alt text](../Praktikum08/images/03.png)
+
+## 2. Implementasi getServerSideProps pada server.tsx
+
+1. Tambah getServerSideProps pada server.tsx
+
+   ![alt text](../Praktikum08/images/04.png)
+
+2. Jalankan browser `http://localhost:3000/produk/server`
+
+   ![alt text](../Praktikum08/images/05.png)
--- Praktikum08/ssr/src/pages/produk/server.tsx
+++ Praktikum08/ssr/src/pages/produk/server.tsx
@@ -1,12 +1,31 @@
 import TampilanProduk from "../views/produk";
 
-const halamanProdukServer = () => {
-    return (
-        <div>
-            <h1>Halaman Produk Server</h1>
-            <TampilanProduk products={[]}/>
-        </div>
-    );
-}
+type ProductType = {
+  id: string;
+  nama: string;
+  harga: number;
+  image: string;
+  kategori: string;
+};
 
-export default halamanProdukServer
+const HalamanProdukServer = ({ products }: { products: ProductType[] }) => {
+  return (
+    <div>
+      <h1>Halaman Produk Server</h1>
+      <TampilanProduk products={products} />
+    </div>
+  );
+};
+
+export default HalamanProdukServer;
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/produk");
+  const response = await res.json();
+
+  return {
+    props: {
+      products: response.data,
+    },
+  };
+}
