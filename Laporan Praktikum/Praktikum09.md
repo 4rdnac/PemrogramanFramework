cab0e1dc07ba88060c6ef0634a987d4cb63d59c2
Candra Ahmad Dani <candraahmaddani04@gmail.com>
2026-03-10 05:49:39 +0700

    P09 Setup Halaman Static

---
+++ Laporan Praktikum/Praktikum09.md
@@ -0,0 +1,11 @@
+# Jobsheet 9
+
+## 1. Setup Halaman Static
+
+1. Buat file baru pada pages/products/static.tsx
+
+   ![alt text](../Praktikum09/images/01.png)
+
+2. Modifikasi file static.tsx
+
+   ![alt text](../Praktikum09/images/02.png)
---
+++ Praktikum09/ssg/src/pages/views/produk/HeroSection.tsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+  return (
+    <div className="text-center text-4xl font-bold mt-8 mb-6 text-blue-600 bg-gray-100 py-6 shadow-md rounded-lg">
+      Katalog Produk
+    </div>
+  );
+};
+
+export default HeroSection;
