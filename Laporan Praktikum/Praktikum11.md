0ce504327af7ef4cb4fff00e46410c55e41e400b
Candra Ahmad Dani <candraahmaddani04@gmail.com>
2026-03-27 06:31:21 +0700

    P11 Tambahkan revalidate

---
+++ Laporan Praktikum/Praktikum11.md
@@ -0,0 +1,7 @@
+# Jobsheet 11
+
+## 1. Tambahkan revalidate
+
+- Buka halaman static.tsx pada folder src/pages/produk
+
+  ![alt text](../Praktikum11/images/01.png)
---
+++ Praktikum11/isr/src/pages/profile/index.tsx
@@ -0,0 +1,14 @@
+const profile = () => {
+  return (
+    <div>
+      <h1>Profile Saya</h1>
+      <br />
+      <p>Nama: Candra Ahmad Dani</p>
+      <p>NIM: 2341720187</p>
+      <p>Program Studi: D4 Teknik Informatika</p>
+      <br />
+    </div>
+  );
+};
+
+export default profile;
